23367675c70eed7d380aae9df7c8bab6faac6fe7
Christian Kaestner <ckaestne@andrew.cmu.edu>
2025-04-04 11:07:37 -0400

    fixed siteswap parsing + tests

--- vizsiteswap/parsing/pattern-fromsiteswap.test.ts
+++ vizsiteswap/parsing/pattern-fromsiteswap.test.ts
@@ -50,35 +50,145 @@ Deno.test("Pattern from siteswap, 756, B starting", () => {
     const t7=s756.findThrow(0)!
     const t5=s756.findThrow(1)!
     const t6=s756.findThrow(2)!
-    assert.equal(t7.toPasserIdxAtThrow, 1);
-    assert.equal(t5.toPasserIdxAtThrow, 0);
-    assert.equal(t6.toPasserIdxAtThrow, 0);
-    assert.equal(t7.fromPasserIdx,0)
-    assert.equal(t5.fromPasserIdx,1)
-    assert.equal(t6.fromPasserIdx,0)
+    assert.equal(t7.toPasserIdxAtThrow, 0);
+    assert.equal(t5.toPasserIdxAtThrow, 1);
+    assert.equal(t6.toPasserIdxAtThrow, 1);
+    assert.equal(t7.fromPasserIdx,1)
+    assert.equal(t5.fromPasserIdx,0)
+    assert.equal(t6.fromPasserIdx,1)
     assert.equal(t7.fromHand,Hand.Right)
-    assert.equal(t5.fromHand,Hand.Right)
+    assert.equal(t5.fromHand,Hand.Left)
     assert.equal(t6.fromHand,Hand.Left)
     assert.equal(s756.getThrowHand(t7, 0), Hand.Right)
-    assert.equal(s756.getThrowHand(t7, 1), Hand.Left)
+    assert.equal(s756.getThrowHand(t7, 1), Hand.Right)
     assert.equal(s756.getThrowHand(t7, 2), Hand.Left)
-    assert.equal(s756.getThrowHand(t7, 3), Hand.Right)
-    assert.equal(s756.getThrowHand(t5, 0), Hand.Right)
+    assert.equal(s756.getThrowHand(t7, 3), Hand.Left)
+    assert.equal(s756.getThrowHand(t5, 0), Hand.Left)
     assert.equal(s756.getThrowHand(t5, 1), Hand.Right)
-    assert.equal(s756.getThrowHand(t5, 2), Hand.Left)
+    assert.equal(s756.getThrowHand(t5, 2), Hand.Right)
     assert.equal(s756.getThrowHand(t5, 3), Hand.Left)
-    assert.equal(t7.isCrossing, true)
-    assert.equal(t5.isCrossing, true)
+    assert.equal(t7.isCrossing, false)
+    assert.equal(t5.isCrossing, false)
     assert.equal(t6.isCrossing, true)
-    assert.equal(s756.getTargetHand(t7, 0), 1-Hand.Right)
+    assert.equal(s756.getTargetHand(t7, 0), Hand.Right)
     assert.equal(s756.getTargetHand(t5, 0), 1-Hand.Right)
     assert.equal(s756.getTargetHand(t7, 1), Hand.Left)
-    assert.equal(s756.getTargetHand(t5, 1), Hand.Right)
-    assert.equal(s756.getTargetHand(t7, 2), 1-Hand.Left)
-    assert.equal(s756.getTargetHand(t5, 2), 1-Hand.Left)
+    assert.equal(s756.getTargetHand(t5, 1), Hand.Left)
+    assert.equal(s756.getTargetHand(t7, 2), Hand.Left)
+    assert.equal(s756.getTargetHand(t5, 2), Hand.Right)
     assert.equal(s756.getTargetHand(t7, 3), Hand.Right)
-    assert.equal(s756.getTargetHand(t5, 3), Hand.Left)
+    assert.equal(s756.getTargetHand(t5, 3), Hand.Right)
 
-    assert.deepEqual(s756.getStartingHands(), [[2, 1], [2,1]])
+    assert.deepEqual(s756.getStartingHands(), [[1, 2], [2,1]])
 })
+
+Deno.test("Pattern from siteswap, 77722", () => {
+
+    const s756 = createSiteswapPatternStr("77722", { startingJuggler: 0 });
+    console.log(s756.prettyPrintThrows())
+
+    const t1= s756.findThrow(0)!
+    const t2= s756.findThrow(1)!
+    assert.equal(t1.fromPasserIdx,0)
+    assert.equal(t2.fromPasserIdx,1)
+    assert.equal(s756.getThrowHand(t1, 0), Hand.Right)
+    assert.equal(s756.getThrowHand(t2, 0), Hand.Right)
+    assert.equal(s756.getThrowHand(t1, 1), Hand.Right)
+    assert.equal(s756.getThrowHand(t2, 1), Hand.Left)
+    assert.equal(s756.getThrowHand(t1, 2), Hand.Left)
+    assert.equal(s756.getThrowHand(t2, 2), Hand.Left)
+    assert.equal(t1.isCrossing, true)
+    assert.equal(t2.isCrossing, false)
+})
+Deno.test("Pattern from siteswap, 77722, B starting", () => {
+
+    const s756 = createSiteswapPatternStr("77722", { startingJuggler: 1 });
+    console.log(s756.prettyPrintThrows())
+
+    const t1= s756.findThrow(0)!
+    const t2= s756.findThrow(1)!
+    assert.equal(t1.fromPasserIdx,1)
+    assert.equal(t2.fromPasserIdx,0)
+    assert.equal(s756.getThrowHand(t1, 0), Hand.Right)
+    assert.equal(s756.getThrowHand(t2, 0), Hand.Left)
+    assert.equal(s756.getThrowHand(t1, 1), Hand.Left)
+    assert.equal(s756.getThrowHand(t2, 1), Hand.Left)
+    assert.equal(s756.getThrowHand(t1, 2), Hand.Left)
+    assert.equal(s756.getThrowHand(t2, 2), Hand.Right)
+    assert.equal(t1.isCrossing, false)
+    assert.equal(t2.isCrossing, true)
+})
+
+
+
+Deno.test("Pattern from siteswap, 7", () => {
+
+    const s756 = createSiteswapPatternStr("7", { startingJuggler: 0 });
+    console.log(s756.prettyPrintThrows())
+
+    const t1= s756.findThrow(0)!
+    assert.equal(t1.fromPasserIdx,0)
+    assert.equal(s756.getThrowHand(t1, 0), Hand.Right)
+    assert.equal(s756.getThrowHand(t1, 1), Hand.Right)
+    assert.equal(s756.getThrowHand(t1, 2), Hand.Left)
+    assert.equal(t1.isCrossing, true)
+})
+
+
+Deno.test("Pattern from siteswap, 75", () => {
+
+    const s756 = createSiteswapPatternStr("75", { startingJuggler: 0 });
+    console.log(s756.prettyPrintThrows())
+
+    const t1= s756.findThrow(0)!
+    const t2= s756.findThrow(1)!
+    assert.equal(t1.fromPasserIdx,0)
+    assert.equal(t2.fromPasserIdx,1)
+    assert.equal(s756.getThrowHand(t1, 0), Hand.Right)
+    assert.equal(s756.getThrowHand(t1, 1), Hand.Left)
+    assert.equal(s756.getThrowHand(t1, 2), Hand.Right)
+    assert.equal(s756.getThrowHand(t2, 0), Hand.Right)
+    assert.equal(s756.getThrowHand(t2, 1), Hand.Left)
+    assert.equal(s756.getThrowHand(t2, 2), Hand.Right)
+    assert.equal(t1.isCrossing, true)
+})
+
+
+Deno.test("Pattern from siteswap, 7575", () => {
+
+    const s756 = createSiteswapPatternStr("7575", { startingJuggler: 0 });
+    console.log(s756.prettyPrintThrows())
+
+    const t1= s756.findThrow(0)!
+    const t2= s756.findThrow(1)!
+    assert.equal(t1.fromPasserIdx,0)
+    assert.equal(t2.fromPasserIdx,1)
+    assert.equal(s756.getThrowHand(t1, 0), Hand.Right)
+    assert.equal(s756.getThrowHand(t1, 1), Hand.Right)
+    assert.equal(s756.getThrowHand(t1, 2), Hand.Right)
+    assert.equal(s756.getThrowHand(t2, 0), Hand.Right)
+    assert.equal(s756.getThrowHand(t2, 1), Hand.Right)
+    assert.equal(s756.getThrowHand(t2, 2), Hand.Right)
+    assert.equal(t1.isCrossing, true)
+})
+
+
+Deno.test("Pattern from siteswap, 756756", () => {
+
+    const s756 = createSiteswapPatternStr("756756", { startingJuggler: 0 });
+    console.log(s756.prettyPrintThrows())
+
+    const t1= s756.findThrow(0)!
+    const t2= s756.findThrow(1)!
+    assert.equal(t1.fromPasserIdx,0)
+    assert.equal(t2.fromPasserIdx,1)
+    assert.equal(s756.getThrowHand(t1, 0), Hand.Right)
+    assert.equal(s756.getThrowHand(t1, 1), Hand.Left)
+    assert.equal(s756.getThrowHand(t1, 2), Hand.Right)
+    assert.equal(s756.getThrowHand(t2, 0), Hand.Right)
+    assert.equal(s756.getThrowHand(t2, 1), Hand.Left)
+    assert.equal(s756.getThrowHand(t2, 2), Hand.Right)
+    assert.equal(t1.isCrossing, true)
+})
+
 
